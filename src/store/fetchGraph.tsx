bb35bb8a63abd10c53abfa37a2fbe2fd8e086f41
afbayonac <afbayonac@gmail.com>
2020-08-15 20:36:13 -0500

    :shirt: Type fechGraph

--- src/store/fetchGraph.tsx
+++ src/store/fetchGraph.tsx
@@ -2,13 +2,13 @@ import { populateGraph } from './modules/graph'
 import axios from 'axios'
 
 const fetchGraph = () => {
-  return dispatch => {
+  return (dispatch: Function) => {
     axios
       .get(`${process.env.REACT_APP_API_POLIORAMA}/perimeter?limit=50`)
       .then(({ data }) => {
         dispatch(populateGraph({
           nodes: data,
-          links: Array(data.length * 3).fill().map(e => ({
+          links: Array(data.length * 3).fill(null).map( _ => ({
             target: data[Math.floor(Math.random() * data.length)]._key,
             source: data[Math.floor(Math.random() * data.length)]._key
           }))
