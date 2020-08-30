423cb357d0b71b84325183e9222cf5e9d05670bf
afbayonac <afbayonac@gmail.com>
2020-08-30 17:03:09 -0500

    :lipstick: Add theme

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,15 @@
-const { override, fixBabelImports } = require('customize-cra')
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const lessToJs = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
+  fixBabelImports('antd', {
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: lessToJs(fs.readFileSync(path.join(__dirname, '/src/theme.less'), 'utf8'))
   })
 )
---
+++ src/components/createBar/createBar.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const CreateBar: React.FC = () => {
+  return (<div>bar</div>);
+};
+
+export default CreateBar;
